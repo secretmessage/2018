754da1d89596fb21c45e7dbebf8245e219017e17
Secret Message <secretmessage@protonmail.com>
2018-04-05 13:23:00 -0400

    add staging port 5000

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 
 cd ~/src/2018;
 wget --no-proxy --recursive --no-parent -w 3 --random-wait http://staging.liftrocket.com
+get --no-proxy --recursive --no-parent -w 3 --random-wait http://staging.liftrocket.com:5000;
 git add .;
 git commit -S;
 git push -u origin --all;
