e33f4f83da8215d6f90d6b913a3ad8555a56d9c2
Secret Message <secretmessage@protonmail.com>
2018-04-05 13:07:43 -0400

    delete techmeme

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/sh 
 
 cd ~/src/2018;
-cd ~/src/2018/techmeme;
-wget --no-proxy --recursive --no-parent -w 3 --random-wait https://techmeme.com;
+cd ~/src/2018/venus;
+wget --no-proxy --recursive --no-parent -w 3 --random-wait http://venus.qc.cuny.edu;
 git add .;
 git commit -S;
 git push -u origin --all;
