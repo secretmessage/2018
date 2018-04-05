6afe0a4ff82b7789623f24bb38f17470ae3c2a06
Secret Message <secretmessage@protonmail.com>
2018-04-05 13:09:01 -0400

    add venus

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 cd ~/src/2018;
 cd ~/src/2018/venus;
-wget --no-proxy --recursive --no-parent -w 3 --random-wait http://venus.qc.cuny.edu;
+wget --no-proxy --recursive --no-parent -w 3 --random-wait http://venus.cs.qc.cuny.edu;
 git add .;
 git commit -S;
 git push -u origin --all;
