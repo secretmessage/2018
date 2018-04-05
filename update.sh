75355e70c198cbf6d45f90e6b3e5b45ad81efb84
Secret Message <secretmessage@protonmail.com>
2018-04-05 15:44:46 -0400

    delete all staging files

--- update.sh
+++ update.sh
@@ -1,8 +1,7 @@
 #!/bin/sh 
 
 cd ~/src/2018;
-wget --no-proxy --recursive --no-parent -w 3 --random-wait http://staging.liftrocket.com
-wget --no-proxy --recursive --no-parent -w 3 --random-wait http://staging.liftrocket.com:5000;
+
 git add .;
 git commit -S;
 git push -u origin --all;
