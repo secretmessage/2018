8666e835a9e0dd72d11097651e443e9cb4bb6c4b
Secret Message <secretmessage@protonmail.com>
2018-04-05 13:13:47 -0400

    remove venus

--- update.sh
+++ update.sh
@@ -1,8 +1,6 @@
 #!/bin/sh 
 
 cd ~/src/2018;
-cd ~/src/2018/venus;
-wget --no-proxy --recursive --no-parent -w 3 --random-wait http://venus.cs.qc.cuny.edu;
 git add .;
 git commit -S;
 git push -u origin --all;
