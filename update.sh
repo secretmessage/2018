6f616c1ef6125bcb5ebcf8fee0c5e9b3bcea5896
Secret Message <secretmessage@protonmail.com>
2018-04-12 15:49:09 -0400

    update shell script
    write everything to root

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/bin/sh 
 
-cd ~/src/2018/780-35/website;
+cd ~/src/2018;
 wget --no-proxy --recursive --no-parent -w 3 --random-wait http://qc-csdept-780:UMLobject-332-class@picasso.cs.qc.cuny.edu/cs780/;
-cd ~/src/2018/765-28/website;
+cd ~/src/2018;
 wget --no-proxy --recursive --no-parent -w 3 --random-wait http://venus.cs.qc.cuny.edu/~smane/;
-cd ~/src/2018/722/website;
+cd ~/src/2018;
 wget --no-proxy --recurisve --no-parent -w 3 --random-wait http://Complexity-cs722:NP-completeness722@picasso.cs.qc.cuny.edu/cs722/;
 cd ~/src/2018;
 git add -A;
